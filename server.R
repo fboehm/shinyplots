2d1421b7fa9381508e4fcd00c56ca299fa19e47a
Frederick Boehm <frederick.boehm@gmail.com>
2021-02-19 15:45:47 -0500

    initial commit

---
+++ server.R
@@ -0,0 +1,18 @@
+library(shiny)
+library(ggplot2)
+
+dat <- readr::read_csv("Chal59_RSTR_Pilot_MASTER.csv")
+
+shinyServer(function(input, output, session) {
+  
+  output$text1 <- renderText({input$id1})
+  
+
+  output$plot1 <- renderPlot({
+    g <- ggplot(dat, aes_string(x = input$select2, y = input$select1)) 
+    g <- g + geom_boxplot() +
+      labs(x = "Class", y = "Value") + 
+      theme_bw()
+    return(g)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,20 @@
+library(shiny)
+dat <- readr::read_csv("Chal59_RSTR_Pilot_MASTER.csv")
+opts <- colnames(dat)
+
+shinyUI(pageWithSidebar(
+  headerPanel('Exploring flow cytometry values'),
+  sidebarPanel(
+    selectInput(inputId = "select1", label = "select", 
+                choices = opts), 
+    selectInput(inputId = "select2", label = "select", 
+                choices = opts)
+    
+  ),
+  mainPanel(
+    
+    textOutput('text1'),
+    textOutput('text2'),
+    plotOutput('plot1')
+  )
+))
